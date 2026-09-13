f9ed49042806fb5e03e80f141e7e19be90ca1d89
Silelv <sil.elv716@gokstadakademiet.no>
2026-09-13 20:46:23 +0200

    stylet knapper og forside

--- src/components/CurrentBet.tsx
+++ src/components/CurrentBet.tsx
@@ -1,5 +1,6 @@
 import { useGame } from "../stores/useGame";
 import { usePlayers } from "../stores/usePlayers";
+import styles from "../componentcss/CurrentBet.module.css";
 
 export default function CurrentBet() {
     const bet = useGame((state) => state.bet);
@@ -12,20 +13,26 @@ export default function CurrentBet() {
     );
 
   return (
-    <div>
-    <p>Innsats: {bet} mynt{bet === 1 ? "" : "er"}</p>
+    <div className={styles.bet}>
+    <p className={styles.amount}>
+        Innsats: {bet} mynt{bet === 1 ? "" : "er"}</p>
+        <div className={styles.buttons}>
     <button
+        className={styles.button}
         type="button"
         onClick={() => setBet(bet - 1)}
         disabled={bet <= 1}>Reduser Innsats
         </button>
+
         <button
-      type="button"
-      onClick={() => setBet(bet + 1)}
-      disabled={!selectedPlayer || bet >= selectedPlayer.coins}
+        className={styles.button}
+        type="button"
+        onClick={() => setBet(bet + 1)}
+        disabled={!selectedPlayer || bet >= selectedPlayer.coins}
     >
       Øk innsats
     </button>
+    </div>
         </div>
     )
 }
