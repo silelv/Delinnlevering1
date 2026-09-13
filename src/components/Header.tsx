d4389396a915c46a0f57ca84f089f4c75f1b9f48
Silelv <sil.elv716@gokstadakademiet.no>
2026-09-13 20:33:04 +0200

    endret bakgrunn og header, med styling på knapper

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,30 @@
 import { NavLink } from "react-router";
+import styles from "../componentcss/Header.module.css"
 
 
 export default function Header() {
   return (
-    <header>
-        <h1>Video poker</h1>
+    <header className={styles.header}>
+        <h1 className={styles.title}>Video poker</h1>
 
-        <nav>
-            <NavLink to="/" end>Spill</NavLink>
-            <NavLink to="/player">Velg spiller</NavLink>
-            <NavLink to="/rules">Spilleregler</NavLink>
+        <nav className={styles.nav} aria-label="Hovedmeny">
+            <NavLink to="/" end
+            className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.active : ""}`
+          } 
+          >
+            Spill</NavLink>
+            <NavLink to="/player" 
+            className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.active : ""}`
+          }
+          >
+            Velg spiller</NavLink>
+            <NavLink to="/rules"  className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.active : ""}`
+          }
+          >
+            Spilleregler</NavLink>
         </nav>
     </header>
   )
